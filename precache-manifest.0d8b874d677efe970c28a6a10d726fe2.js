45002939e39eef579184bc6aaf8736a313834cb1
Jose Hernandez Torres <jose@hernandezt.com>
2019-03-18 19:31:28 -0700

    manual build

---
+++ precache-manifest.0d8b874d677efe970c28a6a10d726fe2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6110a0de11364730edeb",
+    "url": "/BC-Clicky-Game/static/css/main.290ca61a.chunk.css"
+  },
+  {
+    "revision": "6110a0de11364730edeb",
+    "url": "/BC-Clicky-Game/static/js/main.fd6b664f.chunk.js"
+  },
+  {
+    "revision": "850d8aa44a07960495b7",
+    "url": "/BC-Clicky-Game/static/js/runtime~main.468f9e51.js"
+  },
+  {
+    "revision": "1be70b651223b7256769",
+    "url": "/BC-Clicky-Game/static/js/2.5a8e5e20.chunk.js"
+  },
+  {
+    "revision": "387de58b1928527749160c423fd7cf5b",
+    "url": "/BC-Clicky-Game/static/media/Grammi.387de58b.png"
+  },
+  {
+    "revision": "6127f4f2bcb1180daa7864c2034cfd46",
+    "url": "/BC-Clicky-Game/static/media/Zummi.6127f4f2.png"
+  },
+  {
+    "revision": "fa7a9e34a454997e96379294a164deee",
+    "url": "/BC-Clicky-Game/static/media/Tummi.fa7a9e34.png"
+  },
+  {
+    "revision": "023c71045d27e2c6d185444f3e262644",
+    "url": "/BC-Clicky-Game/static/media/Sunni.023c7104.png"
+  },
+  {
+    "revision": "674c3d26030eb8f52248428313f2b72c",
+    "url": "/BC-Clicky-Game/static/media/Gruffi.674c3d26.png"
+  },
+  {
+    "revision": "d5dd34b87d088e2cd07bbe7a7caed885",
+    "url": "/BC-Clicky-Game/static/media/Gusto.d5dd34b8.png"
+  },
+  {
+    "revision": "c1d8a0540b29b11fd49d864319e47e72",
+    "url": "/BC-Clicky-Game/static/media/Ogres.c1d8a054.png"
+  },
+  {
+    "revision": "1bdd7fce87c19dbc04858ff8d58a1351",
+    "url": "/BC-Clicky-Game/static/media/Toadwart.1bdd7fce.png"
+  },
+  {
+    "revision": "63bc57fae8f6bd9a15d60b6dd3577bc2",
+    "url": "/BC-Clicky-Game/static/media/Calla.63bc57fa.png"
+  },
+  {
+    "revision": "f3f9d0d7a3330dcb835c101eb2cbdef7",
+    "url": "/BC-Clicky-Game/static/media/Cavin.f3f9d0d7.png"
+  },
+  {
+    "revision": "d7425d019b0c4836aade731f74400b87",
+    "url": "/BC-Clicky-Game/static/media/Tuxford.d7425d01.png"
+  },
+  {
+    "revision": "7bbf62a1696e17dd91d4506a4f2517ab",
+    "url": "/BC-Clicky-Game/static/media/Igthorn.7bbf62a1.png"
+  },
+  {
+    "revision": "dd4515eb2a83082ee17b0559bc61903c",
+    "url": "/BC-Clicky-Game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/BC-Clicky-Game/precache-manifest.0d8b874d677efe970c28a6a10d726fe2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/BC-Clicky-Game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
